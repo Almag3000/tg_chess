f1a8767df9acfde8b7a938b062a6f9736405cd45
Almag3000 <alim11235813@gmail.com>
2025-06-07 23:14:56 +0300

    Improve puzzle validation and update styles

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -9,7 +9,7 @@ export function Leaderboard() {
     : [];
 
   return (
-    <div className="mt-4">
+    <div className="mt-4 leaderboard">
       <h3 className="text-lg mb-1">Таблица лидеров</h3>
       <ul>
         {users.map((u, i) => (
