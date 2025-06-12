a1f35012b4a7f0c8f2702940c73594277838f291
Almag3000 <alim11235813@gmail.com>
2025-06-12 13:04:28 +0300

    ь

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
